04c296db33abb1035629a2b14e75aeace60c1442
AhmedAbdAlKareem1 <ahmadabdk18@gmail.com>
2026-02-25 23:37:48 +0200

    Update __init__.py

--- experiment_saver/__init__.py
+++ experiment_saver/__init__.py
@@ -1,3 +1,22 @@
-from .saver import ExperimentSaver, ExperimentConfig
+"""
+experiment_saver
 
-__all__ = ["ExperimentSaver", "ExperimentConfig"]
+Unified experiment saving utilities for:
+- TensorFlow / Keras
+- PyTorch
+"""
+
+# TensorFlow saver
+from .saver_tensorflow import ExperimentSaver as TFExperimentSaver
+from .saver_tensorflow import ExperimentConfig as TFExperimentConfig
+
+# PyTorch saver
+from .experiment_saver_torch import ExperimentSaver as TorchExperimentSaver
+from .experiment_saver_torch import ExperimentConfig as TorchExperimentConfig
+
+__all__ = [
+    "TFExperimentSaver",
+    "TFExperimentConfig",
+    "TorchExperimentSaver",
+    "TorchExperimentConfig",
+]
